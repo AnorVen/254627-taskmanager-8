9f7f837763b694c54e5fc0e026cf8b14e89f3045
Georgy.Efremov <georgy.efremov@leomax.ru>
2019-02-26 20:45:19 +0300

    module2 task1 done

---
+++ src/templates/Fliters.js
@@ -0,0 +1,15 @@
+
+ const filterRender = ({TITLE, COUNT = 0, checked = false}) =>
+  `<input type="radio"
+          id="filter__${TITLE.toLowerCase()}"
+          class="filter__input visually-hidden"
+          name="filter"
+          ${checked ? `checked` : ``}
+          ${COUNT === 0 ? `disabled` : ``}
+        />
+        <label for="filter__${TITLE.toLowerCase()}" class="filter__label">
+          ${TITLE.toUpperCase()} <span class="filter__all-count">${COUNT}</span></label
+        >`
+;
+
+export default filterRender
